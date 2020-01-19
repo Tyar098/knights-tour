4b546c01b4b1ff37d7721a2d25047e2182da6ad7
Hikmatyar Mujahidin <hikmatyar@outlook.co.id>
2020-01-19 21:12:58 +0700

    minor fix

--- py/dist/index/backtracking/main.py
+++ py/dist/index/backtracking/main.py
@@ -5,17 +5,20 @@ import threading
 import upsidedown
 from heapq import heappush, heappop
 
+# created and merged by Tyar from various resources
+# NOTE below is for dev
 # n = 5
 # start_x = 0
 # start_y = 0
 
+# pergerakan kuda
 move_x = [2, 1, -1, -2, -2, -1, 1, 2]
 move_y = [1, 2, 2, 1, -1, -2, -2, -1]
 
-# input board size
+# masukkan board size
 n = int(input("board size: "))
 
-# input x
+# masukkan x
 try:
     start_x = int(input("start x : "))
 except ValueError:
@@ -26,7 +29,7 @@ while start_x < 0 or start_x >= n:
     print ("x diluar matriks papan! x harus diantara 0 {0}".format(n))
     start_x = int(input("start x : "))
 
-# input y
+# masukkan y
 try:
     start_y = int(input("start y : "))
 except ValueError:
@@ -37,22 +40,60 @@ while start_y < 0 or start_y >= n:
     print ("y diluar matriks papan! y harus diantara 0 {0}".format(n))
     start_y = int(input("start y : "))
 
-# -- cek apakah x dan y masih didalam matriks
-def isInBoard(x, y, board, n):
 
-    if(x >= 0 and y >= 0 and x < n and y < n and board[x][y] == -1):
-        return True
+# return false jika tidak ada solusi,
+# return true dan exit jika di temukan solusi pertama
+def resolveKTWD(start_x, start_y, n, board, move_x, move_y):
 
-    return False
+    for k in range(n**2):
+        board[start_y][start_x] = k
+        pq = []  # priority queue holder
 
+        for i in range(8):  # mencari jalur
+            new_x = start_x + move_x[i]
+            new_y = start_y + move_y[i]
 
-# -- fungsi untuk print matriks jika terdapat solusi
-def printSolution(board, n):
-    for i in range(n):
-        for j in range(n):
-            print(str(board[i][j]).zfill(2), end=' ')
-        print()
-    # time.sleep(5)
+            if isInBoard(new_y, new_x, board, n):
+                ctr = 0
+                for j in range(8):  # -- mencari derajat dari setiap jalur
+                    ex = new_x + move_x[j]
+                    ey = new_y + move_y[j]
+
+                    if isInBoard(ey, ex, board, n):
+                        ctr += 1
+
+                global countWD
+                countWD += 1
+
+                heappush(pq, (ctr, i)) # push min heap priority queue
+
+        # memindahkan jalur yang mempunyai derajat terkecil
+        if len(pq) > 0:
+            (p, m) = heappop(pq)
+
+            start_x += move_x[m]
+            start_y += move_y[m]
+
+        else:
+            break
+
+    for a in board:
+        if (-1 in a):
+            return False
+
+    return True
+
+# -- fungsi utama
+def solveKTWD():
+    board = [[-1 for i in range(n)]for i in range(n)]
+
+    if (resolveKTWD(start_x, start_y, n, board, move_x, move_y)):
+        for cy in range(n):
+            for cx in range(n):
+                print(str(board[cy][cx]).zfill(2), end=' ')
+            print()
+    else:
+        print("Solution does not exist")
 
 
 # return false jika tidak ada solusi,
@@ -63,17 +104,9 @@ def solveKT():
 
     t = threading.Thread(target=animate)
     t.start()
-    # n = 6
-    # start_x = 0
-    # start_y = 0
-
+    
     # Init Board matrix (list)
     board = [[-1 for i in range(n)]for i in range(n)]
-    # print (board)
-
-    # pergerakan kuda
-    move_x = [2, 1, -1, -2, -2, -1, 1, 2]
-    move_y = [1, 2, 2, 1, -1, -2, -2, -1]
 
     # posisi pertama kuda
     board[start_x][start_y] = 0
@@ -123,7 +156,23 @@ def recurseKT(board, curr_x, curr_y, move_x, move_y, pos, n):
     return False
 
 
-# -- animasi loading
+# -- fungsi helper cek apakah x dan y masih didalam matriks
+def isInBoard(x, y, board, n):
+
+    if(x >= 0 and y >= 0 and x < n and y < n and board[x][y] == -1):
+        return True
+
+    return False
+
+# -- fungsi helper untuk print matriks jika terdapat solusi
+def printSolution(board, n):
+    for i in range(n):
+        for j in range(n):
+            print(str(board[i][j]).zfill(2), end=' ')
+        print()
+    # time.sleep(5)
+
+# -- fungsi helper animasi loading
 def animate():
     asa = upsidedown.transform('  ..')
     for c in itertools.cycle(['..  ', ' : ', asa, ':  ']):
@@ -135,66 +184,12 @@ def animate():
         time.sleep(0.05)
     sys.stdout.write('\rDone!     \r')
 
-# -- flush animasi loading
+# -- fungsi helper flush animasi loading
 def flushLoading():
   return time.sleep(1), sys.stdout.flush()
 
-def resolveKTWD(start_x, start_y, n, board, move_x, move_y):
 
-    for k in range(n**2):
-            # print (k)
-        board[start_y][start_x] = k
-        pq = []  # priority queue of available neighbors
-
-        for i in range(8):  # mencari jalur
-            new_x = start_x + move_x[i]
-            new_y = start_y + move_y[i]
-
-            if isInBoard(new_y, new_x, board, n):
-                ctr = 0
-                for j in range(8):  # -- mencari derajat dari setiap jalur
-                    ex = new_x + move_x[j]
-                    ey = new_y + move_y[j]
-
-                    if isInBoard(ey, ex, board, n):
-                        ctr += 1
-
-                global countWD
-                countWD += 1
-
-                heappush(pq, (ctr, i))
-
-        # memindahkan jalur yang mempunyai derajat terkecil
-        if len(pq) > 0:
-            (p, m) = heappop(pq)
-
-            start_x += move_x[m]
-            start_y += move_y[m]
-
-        else:
-            break
-
-    for a in board:
-        if (-1 in a):
-            return False
-
-    return True
-
-
-def solveKTWD():
-    board = [[-1 for i in range(n)]for i in range(n)]
-
-    if (resolveKTWD(start_x, start_y, n, board, move_x, move_y)):
-        for cy in range(n):
-            for cx in range(n):
-                print(str(board[cy][cx]).zfill(2), end=' ')
-            print()
-    else:
-        print("Solution does not exist")
-
-
-
-# main function
+# main entry function
 if __name__ == "__main__":
     done = False
     count = 0
@@ -216,5 +211,6 @@ if __name__ == "__main__":
     print ("---------------------")
     timeExec = time.perf_counter() - start
     print ("{0} attempts in {1} seconds".format(count, timeExec))
+    print ("---------------------\n")
 
     k=input("press enter to exit") 
